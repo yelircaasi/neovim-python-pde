3884ea9c57109904b689124625f72f78f7dca066
yelircaasi <isaac.r.riley@gmail.com>
2024-11-18 12:51:30 +0000

    add scratch lua files

---
+++ nvim/lua/features/status_line.lua
@@ -0,0 +1,1 @@
+/nix/store/wqblkm1rxipj1552niq0ihm50n5f5z44-home-manager-files/.config/nvim/lua/features/status_line.lua
---
+++ scratch/nvim/init.lua
@@ -0,0 +1,12 @@
+require("colors")
+require("commands")
+require("mappings")
+require("options")
+
+require("features.file_browser_tree")
+require("features.status_line")
+
+
+require("languages.python")
+require("languages.xit")
+
